43d935b1cf6beec74e403d3ca3dcca7d752ecd75
haktan0313 <134720352+haktan313@users.noreply.github.com>
2026-02-12 21:35:13 +0100

    button update

--- Scripts/Button.cs
+++ Scripts/Button.cs
@@ -1,16 +1,17 @@
 
 using System;
+using System.Collections.Generic;
 
 namespace HRealEngine
 {
     public class Button : Entity
     {
-        public string playerTag = "Player";
         public string platformTag = "Platform";
         public Vector3 positionForPlatform = new Vector3(0, 5, 0);
         
         private bool bIsActivated = false;
         private Vector3 originalPlatformPosition = Vector3.Zero;
+        private List<ulong> overlapEntitiesIDs = new List<ulong>();
         
         void OnCreate()
         {
@@ -25,35 +26,44 @@ namespace HRealEngine
         void OnDestroy()
         {
         }
-        void Update()
+        void OnUpdate(float ts)
         {
         }
         void OnCollisionEnter2D(ulong otherID)
         {
             Console.WriteLine("Button collided with entity ID: " + otherID);
-            if (otherID == FindEntityByName(playerTag)?.EntityID)
+            if (!bIsActivated && overlapEntitiesIDs.Count == 0)
             {
                 Console.WriteLine("Button activated by player!");
                 Entity platform = FindEntityByName(platformTag);
                 if (platform != null)
                 {
+                    originalPlatformPosition = platform.Translation;
                     platform.Translation = positionForPlatform;
                     Console.WriteLine("Platform moved to: " + positionForPlatform);
                     bIsActivated = true;
+                    overlapEntitiesIDs.Add(otherID);
                 }
+            }else if (bIsActivated && !overlapEntitiesIDs.Contains(otherID))
+            {
+                overlapEntitiesIDs.Add(otherID);
             }
         }
         void OnCollisionExit2D(ulong otherID)
         {
-            if (bIsActivated && otherID == FindEntityByName(playerTag)?.EntityID)
+            if (bIsActivated && overlapEntitiesIDs.Contains(otherID))
             {
-                Console.WriteLine("Button deactivated by player!");
-                Entity platform = FindEntityByName(platformTag);
-                if (platform != null && bIsActivated)
+                overlapEntitiesIDs.Remove(otherID);
+                if (overlapEntitiesIDs.Count == 0)
                 {
-                    platform.Translation = originalPlatformPosition;
-                    Console.WriteLine("Platform reset to: " + originalPlatformPosition);
-                    bIsActivated = false;
+                    Console.WriteLine("Button deactivated by player!");
+                    Entity platform = FindEntityByName(platformTag);
+                    if (platform != null && bIsActivated)
+                    {
+                        platform.Translation = originalPlatformPosition;
+                        Console.WriteLine("Platform reset to: " + originalPlatformPosition);
+                        bIsActivated = false;
+                    }
                 }
             }
         }
